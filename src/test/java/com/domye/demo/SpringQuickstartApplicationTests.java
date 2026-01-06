ec8eb678d2ae39ee15fa78a0a99aee4ea97fa327
Domye <a1523610551@163.com>
2026-01-06 10:53:27 +0800

    添加Spring Boot项目基础结构和Web控制器

---
+++ src/test/java/com/domye/demo/SpringQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domye.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
